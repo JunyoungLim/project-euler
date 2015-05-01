411d47b4d500ef99e328cad7026f588f98c9897e
Ainodyne <Ainodyne@users.noreply.github.com>
2015-04-30 21:05:52 -0700

    Update Problem001.java

--- Problem001.java
+++ Problem001.java
@@ -12,19 +12,19 @@ package problems;
  */
 public class Problem001
 {
-
-	/**
-	 * 
-	 */
 	public static void run()
 	{
 		System.out.println(sumMultiple(3,999) + sumMultiple(5,999) - sumMultiple(15,999));
 	}
 	
+	
 	public static int sumMultiple(int divisor, int num)
 	{
 		int last = num/divisor;
-    	int sum = (last*(last + 1))/2;
-    	return sum*divisor;
+    		int sum = (last*(last + 1))/2;
+    		return sum*divisor;
 	}
+	
 }
+
+
