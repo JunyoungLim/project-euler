08b3f8d87bdf1ee161585e74c2b56513e996de6d
Ainodyne <Ainodyne@users.noreply.github.com>
2015-04-30 19:45:02 -0700

    Update Problem001.java

--- Problem001.java
+++ Problem001.java
@@ -1,27 +1,15 @@
+
 public class Problem001
 {
-    public static void run2()
+    public static void run()
     {
         System.out.println(sumMultiple(3,999) + sumMultiple(5,999) - sumMultiple(15,999));
     }
     
     public static int sumMultiple(int divisor, int num)
     {
-        int sum = 0;
-        for (int i=0; i <= num; i++)
-        {
-            if (i%divisor == 0) sum += i;
-        }
-        return sum;
-    }
-    
-    public static void run()
-    {
-        int sum = 0;
-        for (int i=3; i < 1000; i++)
-        {
-            if (i%3 == 0 || i%5 == 0) sum += i;
-        }
-        System.out.println(sum);
+        int last = num/divisor;
+        int sum = (last*(last + 1))/2;
+        return sum*divisor;
     }
 }
