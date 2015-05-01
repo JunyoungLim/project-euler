764d51f1936b282c35ece87d671b2d0696ff1326
Ainodyne <Ainodyne@users.noreply.github.com>
2015-04-30 21:44:33 -0700

    Create Problem004.java

---
+++ Problem004.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package problems;
+
+/**
+ * Problem004.java
+ * @author Ainodyne
+ *
+ * @version 1.00
+ * @since Dec 14, 2014
+ */
+public class Problem004
+{
+	public static void run()
+	{
+		int ans = 0;
+		for (int i = 100; i < 1000; i++)
+		{
+			for (int j = 100; j < 1000; j++)
+			{
+				int temp = i*j;
+				if (temp > ans && isPalin(""+temp)) ans = temp;
+				
+			}
+		}
+		System.out.println(ans);
+	}
+	
+	
+	public static boolean isPalin(String n)
+	{
+		return (new StringBuffer(n).reverse().toString().equals(n));
+	}
+
+}
+
+
+// Execution time is 0.020624791 seconds
+
