76161a1cb0611aebd417628680d2afd993c2aba0
Ainodyne <Ainodyne@users.noreply.github.com>
2015-05-03 01:09:26 -0700

    Create Problem007.java

---
+++ Problem007.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package problems;
+
+/**
+ * Problem007.java
+ * @author Ainodyne
+ *
+ * @version 1.00
+ * @since May 3, 2015
+ */
+public class Problem007
+{
+	private static final int TARGET = 10001;
+	
+	public static void run()
+	{
+		int count = 1, num = 0;
+		for (int i=3; count != TARGET; i+=2)
+		{
+			if (isPrime(i)) count++;
+			if (count == TARGET) num = i;
+		}
+		System.out.println(num);
+	}
+	
+	public static boolean isPrime(int n)
+	{
+		if (n == 1) return false;
+		else if (n%2 == 0) return false;
+		for (int i=3, end = (int)Math.sqrt(n); i <= end; i+=2)
+		{
+			if (n%i == 0) return false;
+		}
+		return true;
+	}
+}
+
+
+
+// Answer is 104743
+// Execution time is 0.012449134 seconds
