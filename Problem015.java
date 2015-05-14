6bef3a50403b69cd4985291b9f704ca78ad5edee
Ainodyne <Ainodyne@users.noreply.github.com>
2015-05-13 22:32:46 -0700

    Create Problem015.java

---
+++ Problem015.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package problems;
+
+import java.math.BigInteger;
+
+/**
+ * Problem015.java
+ * @author Ainodyne
+ *
+ * @version 1.00
+ * @since Dec 18, 2014
+ */
+public class Problem015
+{
+	public static void run()
+	{
+		BigInteger num = BigInteger.ONE;
+		
+		for (int i = 21; i <= 40; i++)
+			num = num.multiply(BigInteger.valueOf(i));
+		
+		for (int i = 2; i <= 20; i++)
+			num = num.divide(BigInteger.valueOf(i));
+		
+		System.out.println(num);
+	}
+}
+
+
+// Answer is 137846528820
+// Execution time is 0.003516281 seconds
+
+
