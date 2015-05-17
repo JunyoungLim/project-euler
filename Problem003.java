e8f851401d9825b42acf37f610aef97683450ba8
Ainodyne <Ainodyne@users.noreply.github.com>
2015-05-17 00:09:47 -0700

    Update Problem003.java

--- Problem003.java
+++ Problem003.java
@@ -15,26 +15,32 @@ public class Problem003
 	public static void run()
 	{
 		long d = 600851475143L;
-		int i = 3;
 		
-		if (d%2 == 0) while (d%2 == 0) d /= 2;
-		if (d == 1) i = 4;
-		
-		for ( ; i < Math.sqrt(d); i+=2)
+		for (int i = 3; i < Math.sqrt(d); i+=2)
 		{
 			if (d%i == 0)
 				while (d%i == 0) d /= i;
 		}
-		i -=2 ; // take out unnecessary increment of i at the end of the for-loop
-		if (d == 1) System.out.println(i); // if processed d is in form of p^k where p = prime number and k = integer
-		else System.out.println(d); // if processed d is the largest prime factor
-		
-		// The if-statement for division by 2 at the beginning of this code
-		// and the last if-else statement wasn't necessary for this specific problem
-		// but including the if-else block make it a universal solution
-		// that can be applied to any positive integer
-
+		System.out.println(d);
 	}
+	
+	
+	// universal solution for finding largest prime factor
+	public static int getLargestPrimeFactor(int n)
+    	{		
+		if (n%2 == 0) while (n%2 == 0) n /= 2;
+		if (n == 1) return 2;
+		
+		int i = 3;
+		for ( ; i <= (int)Math.sqrt(n); i+=2)
+		{
+			if (n%i == 0)
+				while (n%i == 0) n /= i;
+		}
+		i -= 2;
+		if (n == 1) return i;
+		return n;
+    	}
 
 }
 
