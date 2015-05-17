042a897c121df92f7e5d288a624a6322de49ef3a
Ainodyne <Ainodyne@users.noreply.github.com>
2015-05-16 22:21:52 -0700

    Update Problem019.java

--- Problem019.java
+++ Problem019.java
@@ -12,48 +12,41 @@ package problems;
  */
 public class Problem019
 {
-
-	/**
-	 * 
-	 */	
 	public static void run()
-    {
-        int count = 0;
-        int day = 2;
-        for (int y=1901; y < 2001; y++)
-        {
-            for (int m=1; m<=12; m++)
-            {
-                if (m == 4 || m == 6 || m == 9 || m == 11)
-                {
-                    day += 30;
-                }
-                else if (m == 2)
-                {
-                	if (isLeap(y)) day += 29;
-                    else day += 28;
-                }
-                else day += 31;
-                
-                day = day%7;
-                if (day == 0) count++;
-            }
-        }
-        System.out.println(count);
-    }
-    
-    public static boolean isLeap(int y)
-    {
-        if (y%4 == 0)
-        {
-            if (y%400 == 0) return true;
-            else if (y%100 == 0) return false;
-            return true;
-        }
-        return false;
-    }
+	{
+		int count = 0, day = 2;
+		
+		for (int y = 1901; y <= 2000; y++)
+		{
+			for (int m = 1; m <= 12; m++)
+			{
+				if (m == 4 || m == 6 || m == 9 || m == 11)
+					day += 30;
+				else if (m == 2)
+				{
+					if (isLeap(y)) day += 29;
+					else day += 28;
+				}
+				else day += 31;
+				
+				day %= 7;
+				if (day == 0) count++;
+			}
+		}
+		System.out.println(count);
+	}
+	
+	public static boolean isLeap(int y)
+	{
+		if (y%4 == 0)
+		{
+			if (y%400 == 0) return true;
+			else if (y%100 == 0) return false;
+			return true;
+		}
+		return false;
+	}
 }
-
 
 
 // Answer is 171
