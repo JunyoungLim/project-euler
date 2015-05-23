95e0b8c3cc5c77a84751d779882d14a17602c58e
Ainodyne <Ainodyne@users.noreply.github.com>
2015-05-22 22:14:58 -0700

    Create Problem014.java

---
+++ Problem014.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package problems;
+
+/**
+ * Problem014.java
+ * @author Ainodyne
+ *
+ * @version 1.00
+ * @since Dec 17, 2014
+ */
+public class Problem014
+{
+	private static final int LIMIT = 1000000;
+	private static int[] collatz = new int[LIMIT+1];
+	private static final int MAX = Integer.MAX_VALUE/3;
+	
+	public static void run()
+	{
+		int num = 0, maxLength = 0;
+		
+		collatz[1] = 1;
+		for (int i = 2; i <= LIMIT; i++)
+		{
+			fillCollatz(i);
+			if (collatz[i] >= maxLength)
+			{
+				maxLength = collatz[i];
+				num = i;
+			}
+		}		
+		System.out.println(num);
+	}
+	
+	public static void fillCollatz(int n)
+	{
+		int count = 0;
+		int current = n;
+		for ( ; current >= n; count++)
+		{
+			if (current%2 == 0) current /= 2;
+			else if (current >= MAX)
+			{
+				long cur = current;
+				for ( ; cur >= MAX; count++)
+				{
+					if (cur%2 == 0) cur /= 2;
+					else cur = cur*3 + 1;
+				}
+				current = (int)cur;
+				count--;
+			}
+			else current = current*3 + 1;
+		}
+		collatz[n] = collatz[current] + count;
+	}
+}
+
+
+
+// Answer is 837799
+// Execution time is 0.080583844 seconds
+
