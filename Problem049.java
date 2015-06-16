83762dc63f17445e230fdd760fc9ecdd5eb5ad2e
Jared Junyoung Lim <Ainodyne@users.noreply.github.com>
2015-06-15 21:34:40 -0700

    Create Problem049.java

---
+++ Problem049.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package problems;
+
+/**
+ * Problem049.java
+ * @author Ainodyne
+ *
+ * @version 1.00
+ * @since May 18, 2015
+ */
+public class Problem049
+{
+	private static final int LIMIT = 10000;
+	private static final boolean[] isPrime = primeList(LIMIT);
+	
+	public static void run()
+	{
+		int num = 0, next1 = 0, next2 = 0;
+		
+		for (int i = 1489; i < LIMIT; i+=2) // 1487 already proven in the problem
+		{
+			if (isPrime[i])
+			{
+				for (int j = 2, end = (LIMIT-i)/2; j < end; j+=2)
+				{
+					int seq1 = i + j, seq2 = seq1 + j;
+					
+					if (isPrime[seq1] && isPrime[seq2])
+						if (isPermutation(i, seq1) && isPermutation(i, seq2))
+						{
+							num = i;
+							next1 = seq1;
+							next2 = seq2;
+						}
+				}
+			}
+			if (num != 0) break;
+		}
+		System.out.println("" + num + next1 + next2);
+	}
+	
+	public static boolean isPermutation(int n, int m)
+	{
+		int[] arr = new int[10];
+		
+		while (n > 0)
+		{
+			arr[n % 10]++;
+			n /= 10;
+		}
+		
+		while (m > 0)
+		{
+			arr[m % 10]--;
+			m /= 10;
+		}
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			if (arr[i] != 0) return false;
+		}
+		return true;
+	}
+	
+	public static boolean[] primeList(int n)
+    {
+        //sieve of Eratosthenes
+        boolean[] prime = new boolean[n+1];
+        java.util.Arrays.fill(prime, 2, prime.length, true);
+        for (int i=4; i < prime.length; i+=2) prime[i] = false;
+        for (int i=3, end = (int)Math.sqrt(n); i <= end; i+=2)
+        {
+            if (prime[i])
+                for (int j=i*i; j <= n; j+=2*i)
+                    prime[j] = false;
+        }
+        return prime;
+    }
+}
+
+
+Answer is 296962999629
+Execution time is 6.169255 ms
+
+
+
