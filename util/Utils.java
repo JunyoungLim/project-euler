e71272ad6c72f72a48427335f574cc1192d18712
Jared Junyoung Lim <jl3248@cornell.edu>
2022-01-07 18:05:16 -0800

    Add utils

--- util/Utils.java
+++ util/Utils.java
@@ -1,2 +1,26 @@
-package util;public class Utils {
+package util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+public class Utils {
+  public static int[][] readMatrix(int N, URL fileURL) {
+    int[][] text = new int[N][N];
+    File file = new File(fileURL.getFile());
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+      String line;
+      for (int i = 0; (line = br.readLine()) != null && i < N; i++) {
+        String[] temp = line.trim().split("\\s+");
+        for (int j = 0; j < N; j++) {
+          text[i][j] = Integer.parseInt(temp[j]);
+        }
+      }
+    } catch (IOException e) {
+      System.err.println(e);
+    }
+    return text;
+  }
 }
